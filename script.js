88bde5e5f14ec5555689eba2a7d561b8e844596f
Nadrojria <loicdesbordes@yahoo.fr>
2025-05-12 16:53:04 +0200

    fin etape 1 : pointe sapin

--- script.js
+++ script.js
@@ -38,24 +38,40 @@ function afficherTriangleGauche(n) {
         console.log(result); 
     }
 }
-  
+// afficherTriangleGauche(4)
+// afficherTriangleDroite(4)
 
-function afficherPointeSapin(hauteur) {
-    //    +
-    //   /|\
-    //  /*|*\
-    // /**|**\
-    ///***|***\
-    
+function afficherPointeSapin(hauteur) {   
     let pointe = "";
     for (let i = 0; i < hauteur; i++) {
         pointe += " ";
     }
-    
     pointe += "+";
-    
     console.log(pointe)
+
+    for (let i = 0; i < hauteur; i++) {  
+        let result = "";
+        for (let j = 0; j < hauteur-i-1; j++) {
+            result += " "
+        }
+        result += "/";
+        for (let k = 0; k < i; k++) {
+            result += "*";
+        }
+        result += "|";
+
+        for (let j = 0; j < i; j++) {
+            result += "*";
+        }
+
+        result += "\\";
+
+        console.log(result)
+    }
+
 }
   
-afficherPointeSapin(4)
+afficherPointeSapin(1)
+afficherPointeSapin(2)
+afficherPointeSapin(3)
 
